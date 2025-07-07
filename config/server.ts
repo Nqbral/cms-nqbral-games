91e3b1a3049506aed5d44c2455b27d21d8ece9ca
Simon Chevalier <sim.chevalier@gmail.com>
2025-07-07 17:33:06 +0200

    Add app keys in server file again (wrong delete)

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,8 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("CMS_URL", "https://cms.nqbral-games.fr"),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
 });
